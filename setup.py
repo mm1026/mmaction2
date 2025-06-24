d991466629bcaff60630ed1945fe6ac907f32aeb
mm1026 <64010628+mm1026@users.noreply.github.com>
2025-06-24 14:33:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,6 @@
 import os
 import os.path as osp
 
-def readme():
-    with open('README.md', encoding='utf-8') as f:
-        content = f.read()
-    return content
-
 def get_version():
     with open(version_file, 'r') as f:
         exec(compile(f.read(), version_file, 'exec'))
